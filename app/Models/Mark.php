2d1677f4f44e0b0962e0b1fb59b08dddea3d6b3c
Boda-M <nagy.zsolt2005@tanulo.boronkay.hu>
2025-03-24 09:31:22 +0100

    nem jo

--- app/Models/Mark.php
+++ app/Models/Mark.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mark extends Model
 {
-    //
+    public $timestamps = false;
+
+    function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
 }
