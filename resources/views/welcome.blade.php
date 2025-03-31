110356251854af694980e2b4c67063e79cc6a2b0
elliotA1ders0n <novak004marton@gmail.com>
2025-03-31 13:18:00 +0200

    evfolyamok kezdes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,9 @@
     <div class="container">
         <h1>Üdvözöljük!</h1>
         <div class="options">
-            <a href="{{ route('login') }}" class="btn">Bejelentkezés</a>
-            <a href="{{ route('register') }}" class="btn">Regisztráció</a>
-            <a href="{{ route('dashboard') }}" class="btn guest-btn">Folytatás bejelentkezés nélkül</a>
+            <a href="{{ route('login') }}" class="btn-login">Bejelentkezés</a>
+            <a href="{{ route('register') }}" class="btn-register">Regisztráció</a>
+            <a href="{{ route('dashboard') }}" class="btn-student">Folytatás tanulóként</a>
         </div>
     </div>
 </body>
