f03a71d3a7ca24e15f8e8b56fb0c1e4050e5720a
Balazs776 <nemecz.balazs.2009@gmail.com>
2025-04-01 12:21:14 +0200

    atlag kezdes

--- resources/views/class_average/index.blade.php
+++ resources/views/class_average/index.blade.php
@@ -4,4 +4,19 @@
             {{ __('Osztályok Átlaga') }}
         </h2>
     </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @foreach($classes->unique('name') as $class)
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div id="items-container" class="mt-4">
+                        {{$class->name}}
+                    </div>
+                </div>
+            </div>
+            <br>
+            @endforeach
+        </div>
+    </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
