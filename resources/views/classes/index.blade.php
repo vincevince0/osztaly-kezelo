7862575ef33f0a791ae848781a80a2926d80a6a8
baloghakos1 <balogh.akos2006@tanulo.boronkay.hu>
2025-04-14 20:53:17 +0200

    most már tényleg merged Áki to main

--- resources/views/classes/index.blade.php
+++ resources/views/classes/index.blade.php
@@ -26,11 +26,7 @@
                             <h3 class="font-semibold text-lg mt-4">{{ request()->get('year_id') }} {{ __(' osztályai: ') }}</h3>
                             <ul>
                                 @foreach($classes->where('year', request()->get('year_id')) as $class)
-<<<<<<< HEAD
                                 <a href="{{ route('classes.show', [$class->year,$class->name]) }}" class="btn-class"><button><i>{{ $class->name }}</i></button></a>
-=======
-                                    <a href="{{ route('login') }}" class="btn-class"><button><i>{{ $class->name }}</i></button></a>   
->>>>>>> Áki
                                 @endforeach
                             </ul>
                         @else
@@ -40,7 +36,6 @@
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
             <div class="py-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -51,7 +46,5 @@
                     </div>
                 </div>
             </div>
-=======
->>>>>>> Áki
     </div>
 </x-app-layout>
